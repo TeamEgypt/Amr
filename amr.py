e89e2c88bce8b4344451a50e751ace34ae302b3d
Team Egypt <60740973+TeamEgypt@users.noreply.github.com>
2020-02-29 02:18:41 +0200

    Create amr.py

---
+++ amr.py
@@ -0,0 +1,50 @@
+import os
+os.system("clear")
+
+import os
+os.system("pip install nexmo") 
+
+import os
+os.system("clear")
+
+import nexmo
+from pprint import pprint
+
+client = nexmo.Client(
+  application_id='88e4e851-f19a-4a5f-84da-80070eb2c5e2',
+  private_key='private.key'
+)
+
+amrtxt = input('Enter the masge: ') 
+amrnumber = input('Enter number: ')
+
+ncco = [
+  {
+    'action': 'talk',
+    'voiceName': 'Maged',
+    'text': amrtxt
+ }
+]
+
+
+response = client.create_call({
+  'to': [{
+    'type': 'phone',
+    'number': amrnumber
+  }],
+  'from': {
+    'type': 'phone',
+    'number': '2012118666'
+
+  },
+  'ncco': ncco
+})
+
+pprint(response)
+
+import os
+os.system("clear")
+
+print("###Done####")
+
+print("designe by Amr Elshrif")
